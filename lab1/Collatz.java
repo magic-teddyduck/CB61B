3b012f84462647f3d1055d70717223f53a5ab880
magic-teddyduck <vincent525352@gmail.com>
2024-02-19 20:29:37 +0800

    Collatz finish

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,11 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        int k = n % 2;
+        if (k  == 0) {
+            return n / 2;
+        } else  {
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
